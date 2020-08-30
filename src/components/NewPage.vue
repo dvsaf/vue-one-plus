ca2e10b23641192c3078e5a37e4a67970816f3bb
Dmitry Safronov <d.v.safronov@live.ru>
2020-08-31 00:47:28 +0300

    Добавлен компонент

---
+++ src/components/NewPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="NewPage">
+    <label for="calendar">Дата</label>
+    <Calendar id="calendar" v-model="value" :showIcon="true"/>
+  </div>
+</template>
+
+<script>
+import Calendar from 'primevue/calendar';
+
+export default {
+  name: "NewPage",
+  components: {
+    Calendar
+  },
+  data() {
+    return {
+      value: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
