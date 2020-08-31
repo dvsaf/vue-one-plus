248c0c7de9e83a3dde238678d6a5d38d5d20a07d
dvsaf <d.v.safronov@gmail.com>
2020-08-31 16:51:46 +0300

    Автодополнение и загрузка данных из сервиса

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -1,43 +1,64 @@
 <template>
-  <div id="user-management">
-    <div class="filter-block">
-      <div class="filer-block-header">Выбрано</div>
-      <div class="filer-block-content">
-        <div v-if="filters.login"><strong>Логин: </strong>{{ filters.login }}</div>
-        <div v-if="filters.lastName"><strong>Фамилия: </strong>{{ filters.lastName }}</div>
-        <div v-if="filters.email"><strong>Email: </strong>{{ filters.email }}</div>
-<!--        <div v-if="filters.college"><strong>ПОО: </strong>{{ filters.college.name }}</div>-->
-        <div class="btn btn-primary" onclick="userManagement.clearFilters()">Очистить</div>
-        <div class="btn btn-primary">Найти</div>
+  <div id="user-management" class="row pt-3 pb-3">
+    <div class="col-3 bg-light">
+      <div class="filter-block">
+        <div class="filer-block-header">Выбрано</div>
+        <div class="filer-block-content">
+          <div v-if="filters.login"><strong>Логин: </strong>{{ filters.login }}</div>
+          <div v-if="filters.lastName"><strong>Фамилия: </strong>{{ filters.lastName }}</div>
+          <div v-if="filters.email"><strong>Email: </strong>{{ filters.email }}</div>
+          <div v-if="filters.college"><strong>ПОО: </strong>{{ filters.college.short_name }}</div>
+          <div class="row">
+            <b-button class="col ml-3 mr-1" @click="clearFilters">Очистить</b-button>
+            <b-button class="col ml-1 mr-3">Найти</b-button>
+          </div>
+        </div>
+      </div>
+      <div class="filter-block filter-block-collapsable">
+        <div class="filer-block-header">Основная информация</div>
+        <div class="filer-block-content">
+          <div>
+            <label for="login">Логин</label>
+            <input id="login" type="text" class="form-control" v-model="filters.login">
+          </div>
+          <div>
+            <label for="last-name">Фамилия</label>
+            <input id="last-name" type="text" class="form-control" v-model="filters.lastName">
+          </div>
+          <div>
+            <label for="email">Email</label>
+            <input id="email" type="text" class="form-control" v-model="filters.email">
+          </div>
+          <div>
+            <label for="college">ПОО</label>
+            <b-form-input id="college" class="form-control" list="colleges" v-model="selectedCollege"
+                          @change="changeCollege"></b-form-input>
+            <datalist id="colleges">
+              <option v-for="college in colleges" v-bind:key="college.id">{{ college.short_name }}</option>
+            </datalist>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="filter-block filter-block-collapsable">
-      <div class="filer-block-header">Основная информация</div>
-      <div class="filer-block-content">
-        <div>
-          <label for="login">Логин</label>
-          <input id="login" v-model="filters.login">
-        </div>
-        <div>
-          <label for="last-name">Фамилия</label>
-          <input id="last-name" v-model="filters.lastName">
-        </div>
-        <div>
-          <label for="email">Email</label>
-          <input id="email" v-model="filters.email">
-        </div>
-<!--        <div>-->
-<!--          <label for="college">ПОО</label>-->
-<!--          <p-autocomplete id="college" v-model="selectedCollege" :dropdown="true" :suggestions="filteredColleges"-->
-<!--                          @complete="searchCollege($event)" @item-select="selectCollege()" @clear="clearCollege()"-->
-<!--                          field="name"></p-autocomplete>-->
-<!--        </div>-->
-      </div>
+    <div class="col-9">
+      hello
     </div>
   </div>
 </template>
 
 <script>
+const collegesUrl = "http://localhost:9081/colleges";
+
+function fetchColleges(self) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", collegesUrl);
+  xhr.onload = function () {
+    self.colleges = JSON.parse(xhr.responseText);
+    console.log("size " + self.colleges);
+  };
+  xhr.send();
+}
+
 export default {
   name: "UserManagement",
 
@@ -52,17 +73,14 @@ export default {
         banned: null
       },
       activeFilters: [],
-      users: [],
-      colleges: [
-        {id: 1, name: 'aaa'},
-        {id: 2, name: 'bbb'},
-        {id: 3, name: 'ccc'},
-        {id: 4, name: 'sad'},
-        {id: 5, name: 'awe'},
-        {id: 6, name: 'era'}],
-      filteredColleges: [],
-      selectedCollege: null
+      selectedCollege: null,
+      colleges: [],
+      users: []
     }
+  },
+
+  created() {
+    fetchColleges(this);
   },
 
   methods: {
@@ -73,20 +91,13 @@ export default {
       this.filters.college = null;
       this.filters.active = true;
       this.filters.banned = null;
+      this.selectedCollege = null;
     },
 
-    searchCollege(event) {
-      this.filteredColleges = this.colleges.filter((college) => {
-        return college.name.toLowerCase().indexOf(event.query.toLowerCase()) >= 0
+    changeCollege(value) {
+      this.filters.college = this.colleges.find((item) => {
+        return item.short_name === value;
       });
-    },
-
-    selectCollege() {
-      this.filters.college = this.selectedCollege;
-    },
-
-    clearCollege() {
-      this.filters.college = null;
     }
   }
 }
