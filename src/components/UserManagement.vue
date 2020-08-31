0b1eda162058cc5ac174fc87c46d2c83d676f180
dvsaf <d.v.safronov@gmail.com>
2020-08-31 17:25:45 +0300

    Добавлены странички и кнопки

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -41,7 +41,17 @@
       </div>
     </div>
     <div class="col-9">
-      <b-table striped hover :items="users"></b-table>
+      <b-table id="users" striped hover :items="users" :fields="userFields" :primary-key="id" :per-page="perPage"
+               :current-page="currentPage">
+        <template v-slot:cell(actions)="data">
+          <b-button class="btn btn-sm">{{ data.item.id }}</b-button>
+        </template>
+        <template v-slot:cell()="data">
+          {{ data.value }}
+        </template>
+      </b-table>
+      <p class="mt-3">Страница: {{ currentPage }}</p>
+      <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage" aria-controls="users"></b-pagination>
     </div>
   </div>
 </template>
@@ -54,7 +64,6 @@ function fetchColleges(self) {
   xhr.open("GET", collegesUrl);
   xhr.onload = function () {
     self.colleges = JSON.parse(xhr.responseText);
-    console.log("size " + self.colleges);
   };
   xhr.send();
 }
@@ -75,12 +84,38 @@ export default {
       activeFilters: [],
       selectedCollege: null,
       colleges: [],
+      userFields: [
+        {key: 'login', label: 'Логин', sortable: true},
+        {key: 'name', label: 'ФИО', sortable: true},
+        {key: 'roles', label: 'Роли', sortable: false},
+        {key: 'actions', label: '', sortable: false}
+      ],
       users: [
-        { id: 0, login: "god", name: "god", roles: "god" },
-        { id: 1, login: "root", name: "root", roles: "root" },
-        { id: 2, login: "operator", name: "operator", roles: "operator" },
-        { id: 3, login: "user", name: "user", roles: "user" }
-      ]
+        {id: 0, login: 'god', name: 'god', roles: 'god'},
+        {id: 1, login: 'root', name: 'root', roles: 'root'},
+        {id: 2, login: 'operator', name: 'operator', roles: 'operator'},
+        {id: 3, login: 'user', name: 'user', roles: 'user'},
+        {id: 10, login: 'god', name: 'god', roles: 'god'},
+        {id: 11, login: 'root', name: 'root', roles: 'root'},
+        {id: 12, login: 'operator', name: 'operator', roles: 'operator'},
+        {id: 13, login: 'user', name: 'user', roles: 'user'},
+        {id: 20, login: 'god', name: 'god', roles: 'god'},
+        {id: 21, login: 'root', name: 'root', roles: 'root'},
+        {id: 22, login: 'operator', name: 'operator', roles: 'operator'},
+        {id: 23, login: 'user', name: 'user', roles: 'user'},
+        {id: 30, login: 'god', name: 'god', roles: 'god'},
+        {id: 31, login: 'root', name: 'root', roles: 'root'},
+        {id: 32, login: 'operator', name: 'operator', roles: 'operator'},
+        {id: 33, login: 'user', name: 'user', roles: 'user'}
+      ],
+      perPage: 7,
+      currentPage: 1
+    }
+  },
+
+  computed: {
+    rows() {
+      return this.users.length
     }
   },
 
